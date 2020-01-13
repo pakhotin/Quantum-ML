f32eadb210cfc5036e1fbe728992926e2374ebf3
Yuriy Pakhotin <yuriy.pakhotin@gm.com>
2020-01-13 15:41:22 -0600

    Initial submission

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Quantum ML',
+    author='Yuriy Pakhotin',
+    license='MIT',
+)
